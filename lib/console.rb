aa9de648e1f0e92643b1a15a7bf52acf407dcf91
woink <wardprice@gmail.com>
2020-07-16 18:57:33 -0400

    #waiters and #best_tipper not passing

---
+++ lib/console.rb
@@ -0,0 +1,13 @@
+require_relative './customer.rb'
+require_relative './meal.rb'
+require_relative './waiter.rb'
+
+require 'pry'
+
+
+
+
+
+
+
+# binding.pry
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,26 @@
 class Waiter
+ 
+    attr_accessor :name, :yrs_experience
+   
+    @@all = []
+   
+    def initialize(name, yrs_experience)
+      @name = name
+      @yrs_experience = yrs_experience
+      @@all << self
+    end
+   
+    def self.all
+      @@all
+    end
+   
+    def new_meal(customer, total, tip=0)
+        Meal.new(self, customer, total, tip)
+      end
+      
+      def meals
+        Meal.all.select { |meal| meal.waiter == self }
+      end
 
-end
+      
+  end
